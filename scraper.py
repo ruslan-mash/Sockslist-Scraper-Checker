323ad22beb557b5f281923a2df2609e2d2911b2e
regresmarsh@gmail.com <regresmarsh@gmail.com>
2024-12-08 01:02:38 +0300

    скрапер с requests

--- scraper.py
+++ scraper.py
@@ -1,22 +1,17 @@
-# socks-scraper-checker
-# import aiohttp
-# import asyncio
+# socks5-scraper-checker
+
 import requests
 import re
 import time
 
 start_time = time.time()
 
-# Источники прокси socks 4,5 в формате txt из Интернета
+# Источники прокси socks 5 в формате txt из Интернета
 txt_sources = (
     "https://spys.me/socks.txt",
     "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks5/data.txt",
-    # "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks4/data.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
-    # "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
     "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks5/socks5.txt",
-    # "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks4/socks4.txt",
-    # "https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks4.txt",
     "https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks5.txt",
 )
 # Паттерн для фильтрации по ip:port из источника
@@ -34,7 +29,7 @@ validated_socks = []  # список валидированнных прокси
 def get_all_proxies():
     for url in txt_sources:
         try:
-            proxies = requests.get(url=url, headers=header)  # получаем текст из конкретного источника
+            proxies = requests.get(url=url, headers=header)  # получаем список ip:port из источника
             proxy_list = proxies.text.splitlines()  # разделяем ответ на строки
             proxy_list_filtered = re.findall(pattern, "\n".join(
                 proxy_list))  # Объединяем строки списка в единую строку для применения паттерна поиска и применяем паттерн поиска
@@ -54,27 +49,27 @@ print(f'Найдено {len(founded_proxy)} прокси')
 print(f'Ранее удалено {len(proxies_list) - len(founded_proxy)} дубликатов')
 
 
-# Проверка найденных прокси на работоспособность
+# Проверка найденных прокси на работоспособность через google
 def check_proxy():
-    for proxy in founded_proxy:  # составляем словарик для проверки прокси и передачи в requests, если прокси с портом 1080 то это socks4, иначе socks5
-        for proxy in founded_proxy:
-            proxy_dict = {
-                'http': f'socks5://{proxy}',  # Рассматриваем все прокси как socks5
-                'https': f'socks5://{proxy}'
-            }
-
+    for proxy in proxies_list:
+        proxy_dict = {
+            'http': f'socks5://{proxy}',  # Рассматриваем все прокси как socks5
+            'https': f'socks5://{proxy}'
+        }
+        print(proxy_dict)
         try:
-            resp = requests.get(url='https://www.google.com', headers=header, proxies=proxy_dict, timeout=5)
-            print(proxy_dict, resp.status_code)
+            resp = requests.get(url='https://www.google.com', headers=header, proxies=proxy_dict, timeout=3.05)
             if resp.status_code == 200:
                 validated_socks.append(proxy)
-        except requests.exceptions.RequestException:
-            pass
+                print(f"Proxy {proxy} is valid, status code: {resp.status_code}")  # принт о прохождении
+        except requests.exceptions.RequestException as e:
+            continue
+
 
 # Вызов функции для проверки прокси
 check_proxy()
 
-# Принт промежуточная проверка результатов
+# принты проверки выполнения
 print(f'Валидировано {len(validated_socks)} прокси')
 
 # Время выполнения операции
