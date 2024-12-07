52faa0604e5ac905422ba00a88c13227cb1512fe
regresmarsh@gmail.com <regresmarsh@gmail.com>
2024-12-07 23:33:47 +0300

    скрапер с проверкой

--- scraper.py
+++ scraper.py
@@ -7,101 +7,75 @@ import time
 
 start_time = time.time()
 
-# Источники
-spys_source = "https://spys.me/socks.txt"
-proxifly_sources = ("https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks5/data.txt",
-                    "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks4/data.txt")
+# Источники прокси socks 4,5 в формате txt из Интернета
 txt_sources = (
+    "https://spys.me/socks.txt",
+    "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks5/data.txt",
+    # "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks4/data.txt",
     "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks5.txt",
-    "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
+    # "https://raw.githubusercontent.com/TheSpeedX/SOCKS-List/master/socks4.txt",
     "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks5/socks5.txt",
-    "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks4/socks4.txt",
-    "https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks4.txt",
+    # "https://raw.githubusercontent.com/officialputuid/KangProxy/KangProxy/socks4/socks4.txt",
+    # "https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks4.txt",
     "https://raw.githubusercontent.com/r00tee/Proxy-List/main/Socks5.txt",
 )
+# Паттерн для фильтрации по ip:port из источника
 pattern = r'(\d+\.\d+\.\d+\.\d+:\d+)'
+# Заголовок для маскировки под браузер
 header = ({
     'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
-proxies_list = []
-validated_socks = []
+
+proxies_list = []  # список найденных прокси
+validated_socks = []  # список валидированнных прокси
 
 
 # Извлекаем все прокси из списка URL-адресов источников прокси
-# Extract proxies from all source URLs
+
 def get_all_proxies():
     for url in txt_sources:
         try:
-            proxies = requests.get(url=url, headers=header)
-            proxy_list = proxies.text.splitlines()  # Split the response into individual lines
-            proxy_list_filtered = re.findall(pattern, "\n".join(proxy_list))  # Join lines into a single string and apply regex
+            proxies = requests.get(url=url, headers=header)  # получаем текст из конкретного источника
+            proxy_list = proxies.text.splitlines()  # разделяем ответ на строки
+            proxy_list_filtered = re.findall(pattern, "\n".join(
+                proxy_list))  # Объединяем строки списка в единую строку для применения паттерна поиска и применяем паттерн поиска
 
-            proxies_list.extend(proxy_list_filtered)  # Add filtered proxies to the proxies_list
+            proxies_list.extend(proxy_list_filtered)  # добавляем отфильтрованные данные в proxies_list
 
         except requests.exceptions.RequestException as e:
             raise Exception(f"Error fetching proxies from {url}: {e}")
 
-# Call the function to get proxies
+
+# Вызов функции для получения прокси
 get_all_proxies()
-founded_proxy=tuple(set(proxies_list))
+founded_proxy = tuple(set(proxies_list))  # удаляем дубликаты
 
-#Print all the proxies collected
-print("\nAll collected proxies:")
-for proxy in founded_proxy:
-    print(proxy)
-print(f'найдено {len(founded_proxy)} прокси')
+# Принт промежуточная проверка результатов
+print(f'Найдено {len(founded_proxy)} прокси')
+print(f'Ранее удалено {len(proxies_list) - len(founded_proxy)} дубликатов')
 
 
-#
-#
-#
-#
-#
-#
-#
-# tch_proxies_from_url(session, url) for url in proxy_sources]
-#         results = await asyncio.gather(*tasks)
-#
-#         # Свести список списков прокси в один список прокси
-#         all_proxies = list(set([proxy for sublist in results for proxy in sublist]))
-#         print(f"Количество полученных прокси: {len(all_proxies)}")
-#         print(f"Удалено {len([proxy for sublist in results for proxy in sublist]) - len(all_proxies)} дупликатов")
-#         return all_proxies
-#
-#
-# # Проверка каждого прокси из полученного списка, чтобы убедиться, что он работает
-# async def get_working_socks(session, proxy):
-#     try:
-#         async with session.get(f'http://{proxy}', timeout=5) as resp:
-#             if resp.status == 200:
-#                 print(f"Прокси {proxy} работает.")
-#                 working_socks.append(proxy)
-#             else:
-#
-#
-#
-# async def get_all_proxies():
-#     async with aiohttp.ClientSession() as session:
-#         tasks = [fe
-#                 print(f"Прокси {proxy} не работает. Статус: {resp.status}")
-#     except Exception as e:
-#         print(f"Ошибка при проверке прокси {proxy} : {e}")
-#
-#
-# # Основная функция для получения всех прокси и проверки их работы
-# async def main():
-#     async with aiohttp.ClientSession() as session:
-#         proxies = await get_all_proxies()  # Получение всех прокси из источников
-#         tasks = [get_working_socks(session, proxy) for proxy in proxies]  # Проверка работает ли каждый прокси
-#         await asyncio.gather(*tasks)
-#
-#
-# # Запускаем цикл событий asyncio
-# if __name__ == "__main__":
-#     asyncio.run(main())
-#
-#     # Количество рабочих прокси
-#     print(f'Найдено {len(working_socks)} рабочих прокси')
-#     print('\n'.join(working_socks))
+# Проверка найденных прокси на работоспособность
+def check_proxy():
+    for proxy in founded_proxy:  # составляем словарик для проверки прокси и передачи в requests, если прокси с портом 1080 то это socks4, иначе socks5
+        for proxy in founded_proxy:
+            proxy_dict = {
+                'http': f'socks5://{proxy}',  # Рассматриваем все прокси как socks5
+                'https': f'socks5://{proxy}'
+            }
+
+        try:
+            resp = requests.get(url='https://www.google.com', headers=header, proxies=proxy_dict, timeout=5)
+            print(proxy_dict, resp.status_code)
+            if resp.status_code == 200:
+                validated_socks.append(proxy)
+        except requests.exceptions.RequestException:
+            pass
+
+# Вызов функции для проверки прокси
+check_proxy()
+
+# Принт промежуточная проверка результатов
+print(f'Валидировано {len(validated_socks)} прокси')
 
 # Время выполнения операции
 print("--- %s секунд затрачено на выполнение ---" % (time.time() - start_time))
