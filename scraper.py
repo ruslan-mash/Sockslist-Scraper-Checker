ba1439c686edb84569a89291e222d412d032e02d
regresmarsh@gmail.com <regresmarsh@gmail.com>
2024-12-08 13:26:14 +0300

    перед проверкой вс.08.12 13.25 .

--- scraper.py
+++ scraper.py
@@ -4,9 +4,10 @@ import requests
 import re
 import time
 
+
 start_time = time.time()
 
-# Источники прокси socks 5 в формате txt из Интернета
+# Источники прокси socks5 в формате txt из Интернета
 txt_sources = (
     "https://spys.me/socks.txt",
     "https://raw.githubusercontent.com/proxifly/free-proxy-list/main/proxies/protocols/socks5/data.txt",
@@ -17,8 +18,9 @@ txt_sources = (
 # Паттерн для фильтрации по ip:port из источника
 pattern = r'(\d+\.\d+\.\d+\.\d+:\d+)'
 # Заголовок для маскировки под браузер
-header = ({
-    'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'})
+header = {
+    'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4472.124 Safari/537.36'
+}
 
 proxies_list = []  # список найденных прокси
 validated_socks = []  # список валидированнных прокси
@@ -48,20 +50,21 @@ founded_proxy = tuple(set(proxies_list))  # удаляем дубликаты
 print(f'Найдено {len(founded_proxy)} прокси')
 print(f'Ранее удалено {len(proxies_list) - len(founded_proxy)} дубликатов')
 
-
-# Проверка найденных прокси на работоспособность через google
-def check_proxy():
-    for proxy in proxies_list:
+# Проверка найденных прокси на работоспособность через url
+def check_proxy(check_proxy_count=0):
+    for proxy in founded_proxy:
         proxy_dict = {
             'http': f'socks5://{proxy}',  # Рассматриваем все прокси как socks5
             'https': f'socks5://{proxy}'
         }
-        print(proxy_dict)
+        check_proxy_count += 1 #считалка потом удалить
+        print(
+            f' Проверяется {proxy_dict}, всего проверено {check_proxy_count} из {len(founded_proxy)}, прошло  {(time.time() - start_time)//3600} часов {(time.time() - start_time)//60} минут {(time.time() - start_time)%60} секунд')  # потом удалить
         try:
-            resp = requests.get(url='https://www.google.com', headers=header, proxies=proxy_dict, timeout=3.05)
+            resp = requests.get(url='https://reqres.in/api/users/2', headers=header, proxies=proxy_dict, timeout=3.02)
             if resp.status_code == 200:
                 validated_socks.append(proxy)
-                print(f"Proxy {proxy} is valid, status code: {resp.status_code}")  # принт о прохождении
+                print(f"Proxy {proxy} is valid, status code: {resp.status_code}")  # принт о прохождении потом удалить
         except requests.exceptions.RequestException as e:
             continue
 
@@ -72,5 +75,18 @@ check_proxy()
 # принты проверки выполнения
 print(f'Валидировано {len(validated_socks)} прокси')
 
-# Время выполнения операции
-print("--- %s секунд затрачено на выполнение ---" % (time.time() - start_time))
+# Время выполнения всей программы
+end_time = time.time()
+elapsed_time = end_time - start_time
+minutes = elapsed_time // 60
+seconds = elapsed_time % 60
+print(f"--- {int(minutes)} мин {seconds:.2f} секунд затрачено на выполнение всей программы ---")
+
+# Запись валидированных прокси в файл
+with open('socks5_proxies.txt', 'w') as file:
+    for proxy in validated_socks:
+        file.write(f"{proxy}\n")
+
+print("Валидированные прокси записаны в файл socks5_proxies.txt") #потом удалить
+
+
